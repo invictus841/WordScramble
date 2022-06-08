b50e7bdf50b959bcfc7bc4ee93ad4baf4a258ff9
NT <usedforfunny@gmail.com>
2022-06-08 16:15:44 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alexandre Talatinian on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
